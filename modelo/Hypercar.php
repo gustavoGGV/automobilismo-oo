2d350886bc1d1bf82abf855090388b8c2e50855f
Gustavo <134728517+gustavoGGV@users.noreply.github.com>
2024-11-21 14:46:36 -0300

    Update Hypercar.php

--- modelo/Hypercar.php
+++ modelo/Hypercar.php
@@ -1,11 +1,61 @@
 <?php
 
 require_once("IUsarCarro.php");
-require_once("CarroCorrida.php");
+require_once("Carro.php");
 
-class Hypercar extends CarroCorrida {
+class Hypercar extends Carro implements IUsarCarro {
 
     private string $bop;
+
+    public function __toString() {
+
+        return "Modelo: " . $this->modelo . "\nFabricante: " . $this->fabricante . "\nMotor: " . $this->motor. "\nNome da equipe: " . $this->equipe->getNome() . "\nPiloto: " . $this->equipe->getPiloto() . "\nBoP: " . $this->bop . "\n\n";
+
+    }
+
+    public function ligarCarro() {
+
+        return "\nSeu Hypercar de BoP " . $this->bop . " ligou! Você aguenta pilotar por 24 horas?\n";
+
+    }
+
+    public function pilotarCarro($circuitos, $opcao_circuito) {
+
+        if($this->bop=="Ruim") {
+
+            if($circuitos[$opcao_circuito]->getNomeComum()=="Interlagos") {
+
+                return "\nSeu Hypercar de BoP " . $this->bop . " fez um tempo bem medíocre para os seus padrões!\n";
+    
+            } else if($circuitos[$opcao_circuito]->getNomeComum()=="Daytona") {
+    
+                return "\nQue combinação é essa, cara?\n";
+    
+            } else {
+    
+                return "\nSeu Hypercar de BoP " . $this->bop . " fez um tempo medíocre para seus padrões, mas viva LeMans!\n";
+    
+            }
+
+        } else {
+
+            if($circuitos[$opcao_circuito]->getNomeComum()=="Interlagos") {
+
+                return "\nSeu Hypercar de BoP " . $this->bop . " fez um tempo excelente para os seus padrões!\n";
+    
+            } else if($circuitos[$opcao_circuito]->getNomeComum()=="Daytona") {
+    
+                return "\nQue combinação é essa, cara?\n";
+    
+            } else {
+    
+                return "\nSeu Hypercar de BoP " . $this->bop . " fez um tempo extremamente excelente para os seus padrões, viva LeMans!\n";
+    
+            }
+
+        }
+
+    }
 
     /**
      * Get the value of bop
