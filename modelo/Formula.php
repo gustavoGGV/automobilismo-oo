c330eeaff70d55b89d89442d184a059ac9f15c78
Gustavo <134728517+gustavoGGV@users.noreply.github.com>
2024-11-21 14:46:22 -0300

    Update Formula.php

--- modelo/Formula.php
+++ modelo/Formula.php
@@ -1,28 +1,59 @@
 <?php
 
 require_once("IUsarCarro.php");
-require_once("CarroCorrida.php");
+require_once("Carro.php");
+require_once("Circuito.php");
 
-class Formula extends CarroCorrida {
+class Formula extends Carro implements IUsarCarro {
 
-    private int $anoEpoca;
+    private int $ano;
 
-    /**
-     * Get the value of anoEpoca
-     */ 
-    public function getAnoEpoca()
-    {
-        return $this->anoEpoca;
+    public function __toString() {
+
+        return "Modelo: " . $this->modelo . "\nFabricante: " . $this->fabricante . "\nMotor: " . $this->motor. "\nNome da equipe: " . $this->equipe->getNome() . "\nPiloto: " . $this->equipe->getPiloto() . "\nAno: " . $this->ano . "\n\n";
+
+    }
+
+    public function ligarCarro() {
+
+        return "\nSeu fórmula do ano " . $this->ano . " ligou seu motor " . $this->motor . "!\n";
+
+    }
+
+    public function pilotarCarro($circuitos, $opcao_circuito) {
+
+        if($circuitos[$opcao_circuito]->getNomeComum()=="Interlagos") {
+
+            return "\nSeu fórmula de ano " . $this->ano . " fez um tempo extremamente rápido!\n";
+
+        } else if($circuitos[$opcao_circuito]->getNomeComum()=="Daytona") {
+
+            return "\nSeu fórmula de ano " . $this->ano . " ferveu muito por conta do RPM alto por muito tempo, motor perdido (e o carro inteiro)! Carro deletado!\n";
+
+        } else {
+
+            return "\nSeu fórmula de ano " . $this->ano . " fez um tempo extremamente rápido, mas quase conheceu a morte inúmeras vezes!\n";
+
+        }
+
     }
 
     /**
-     * Set the value of anoEpoca
+     * Get the value of ano
+     */ 
+    public function getAno()
+    {
+        return $this->ano;
+    }
+
+    /**
+     * Set the value of ano
      *
      * @return  self
      */ 
-    public function setAnoEpoca($anoEpoca)
+    public function setAno($ano)
     {
-        $this->anoEpoca = $anoEpoca;
+        $this->ano = $ano;
 
         return $this;
     }
